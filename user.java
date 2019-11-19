9b7b127a1f7e65656b77368046cde663cc8f1051
MiestnyLovecZien <48026495+MiestnyLovecZien@users.noreply.github.com>
2019-11-19 19:28:49 +0000

    Delete user.java

--- user.java
+++
@@ -1,9 +0,0 @@
-public class user{
-    String user;
-    public user(String user){
-        if(user.toUpperCase() == "SUPERVISOR"){this.user="SUPERVISOR";
-        }else if(user.toUpperCase() == "DESK"){this.user="DESK";
-        }else if(user.toUpperCase() == "CUSTOMER"){this.user="USER";
-        }
-    }
-}

9b2a7538f12f203aa06b9bc23e5adcd9e6a8585c
SpiderLover69 <48069484+SpiderLover69@users.noreply.github.com>
2019-11-25 19:33:09 +0000

    Add files via upload

--- toReadCSV.java
+++ toReadCSV.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.util.Scanner;
 public class toReadCSV{
     private int x;
+    private int resNum = 0;
 
     public static void readRes(int resNum) throws Exception { //prints out info from a csv file
         // Create a File instance
@@ -47,5 +48,149 @@ public class toReadCSV{
         }
         return numberofLines;
     }
+    public static String readResType(int resNum) throws Exception { //prints out info from a csv file
+        String resType = "";
+        
+        // Create a File instance
+        java.io.File file = new java.io.File("outputFile.csv");
 
+        // Create a Scanner for the file
+        Scanner input = new Scanner(file);
+
+        // Read data from a file
+        while (input.hasNext()) {
+            int isResNum = input.nextInt();
+            if(isResNum == resNum) {
+                String firstName = input.next();
+                String lastName = input.next();
+                resType = input.next();
+                String startDate = input.next();
+                String endDate = input.next();
+                String created = input.next();
+                System.out.println(
+                firstName + " " + lastName + " " + resType + " " + startDate + " " +endDate+ " " +created);
+                input.close();
+            }
+            
+            // Close the file
+            //input.close();
+        }
+        return resType;
+    }
+    public static String readResLastName(int resNum) throws Exception { //prints out info from a csv file
+        String lastName = "";
+        
+        // Create a File instance
+        java.io.File file = new java.io.File("outputFile.csv");
+
+        // Create a Scanner for the file
+        Scanner input = new Scanner(file);
+
+        // Read data from a file
+        while (input.hasNext()) {
+            int isResNum = input.nextInt();
+            if(isResNum == resNum) {
+                String firstName = input.next();
+                lastName = input.next();
+                String resType = input.next();
+                String startDate = input.next();
+                String endDate = input.next();
+                String created = input.next();
+                System.out.println(
+                firstName + " " + lastName + " " + resType + " " + startDate + " " +endDate+ " " +created);
+                input.close();
+            }
+            
+            // Close the file
+            //input.close();
+        }
+        return lastName;
+    }
+    public static String readResFirstName(int resNum) throws Exception { //prints out info from a csv file
+        String firstName = "";
+        
+        // Create a File instance
+        java.io.File file = new java.io.File("outputFile.csv");
+
+        // Create a Scanner for the file
+        Scanner input = new Scanner(file);
+
+        // Read data from a file
+        while (input.hasNext()) {
+            int isResNum = input.nextInt();
+            if(isResNum == resNum) {
+                firstName = input.next();
+                String lastName = input.next();
+                String resType = input.next();
+                String startDate = input.next();
+                String endDate = input.next();
+                String created = input.next();
+                System.out.println(
+                firstName + " " + lastName + " " + resType + " " + startDate + " " +endDate+ " " +created);
+                input.close();
+            }
+            
+            // Close the file
+            //input.close();
+        }
+        return firstName;
+    }
+    public static String readResStart(int resNum) throws Exception { //prints out info from a csv file
+        String startDate = "";
+        
+        // Create a File instance
+        java.io.File file = new java.io.File("outputFile.csv");
+
+        // Create a Scanner for the file
+        Scanner input = new Scanner(file);
+
+        // Read data from a file
+        while (input.hasNext()) {
+            int isResNum = input.nextInt();
+            if(isResNum == resNum) {
+                String firstName = input.next();
+                String lastName = input.next();
+                String resType = input.next();
+                startDate = input.next();
+                String endDate = input.next();
+                String created = input.next();
+                System.out.println(
+                firstName + " " + lastName + " " + resType + " " + startDate + " " +endDate+ " " +created);
+                input.close();
+            }
+            
+            // Close the file
+            //input.close();
+        }
+        return startDate;
+    }
+    public static String readResEnd(int resNum) throws Exception { //prints out info from a csv file
+        String endDate = "";
+        
+        // Create a File instance
+        java.io.File file = new java.io.File("outputFile.csv");
+
+        // Create a Scanner for the file
+        Scanner input = new Scanner(file);
+
+        // Read data from a file
+        while (input.hasNext()) {
+            int isResNum = input.nextInt();
+            if(isResNum == resNum) {
+                String firstName = input.next();
+                String lastName = input.next();
+                String resType = input.next();
+                String startDate = input.next();
+                endDate = input.next();
+                String created = input.next();
+                System.out.println(
+                firstName + " " + lastName + " " + resType + " " + startDate + " " +endDate+ " " +created);
+                input.close();
+            }
+            
+            // Close the file
+            //input.close();
+        }
+        return endDate;
+    }
 }
